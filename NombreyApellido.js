34b84d19db31b8341a38af80cbfa746d4f2f9f7d
Bruno <piaggiobruno91@gmail.com>
2023-08-27 18:03:16 -0300

    first commit

---
+++ NombreyApellido.js
@@ -0,0 +1,25 @@
+// Definimos las variables
+var nombre = "Nombre";
+var apellido = "Apellido";
+
+// Calcular y mostrar la suma
+console.log(nombre + " " + apellido);
+
+// Definimos dos variables
+var numero1 = 5;
+var numero2 = 10;
+
+// Calcula la suma
+var suma = numero1 + numero2;
+
+// Mostrar la suma
+console.log("El resultado de la suma de los números es:", suma);
+
+// Variable 3
+var numero3 = 7;
+
+// Hace la suma entre la variable "suma" y "numero3"
+var resultado = suma + numero3;
+
+// Mostrar el resultado
+console.log("El resultado de la segunda suma es:", resultado);
